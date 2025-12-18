825554d6eba734e671586eae0c3f7557e3c8dafd
Paul van Aarle <paul@animeerwaarde.nl>
2025-12-18 18:44:06 +0100

    fase 9, 2 files vergeten

---
+++ LovionIntegrationClient.Infrastructure/Migrations/20251218155316_AddImportErrorSeverity.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LovionIntegrationClient.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImportErrorSeverity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Severity",
+                table: "ImportErrors",
+                type: "TEXT",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Severity",
+                table: "ImportErrors");
+        }
+    }
+}
